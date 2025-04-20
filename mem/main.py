59aeb3a733bc1a426706345a60ee012b0197caac
Thomas McKee <caffeine.storm@gmail.com>
2025-04-20 13:46:19 -0400

    add vertical numberline

--- mem/main.py
+++ mem/main.py
@@ -17,9 +17,15 @@ class Memory(Scene):
 		kwargs['x_range'] = [0, 16, 1]
 		posl = NumberLine(**kwargs)
 
+		kwargs['rotation'] = -PI/2
+		kwargs['label_direction'] = LEFT
+		vertl = NumberLine(**kwargs)
+
 		self.play(Create(dashl))  # animate the creation of the numberline
 		self.play(Transform(dashl, nl, replace_mobject_with_target_in_scene=True))
 		self.pause(2)
 		self.play(Transform(nl, posl, replace_mobject_with_target_in_scene=True))
 		self.pause(2)
-		self.play(FadeOut(posl))  # fade out animation
+		self.play(Transform(posl, vertl, replace_mobject_with_target_in_scene=True))
+		self.pause(2)
+		self.play(FadeOut(vertl))  # fade out animation
